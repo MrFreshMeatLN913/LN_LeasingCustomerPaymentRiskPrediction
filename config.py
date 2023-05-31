85becf2addfa9ac7e98b2bce268b85a0be795b72
MrFreshMeatLN913 <1721079@qq.com>
2023-05-31 17:59:05 +0800

    modify...1

--- config.py
+++ config.py
@@ -15,12 +15,12 @@ train_params = {
 }
 
 dataset_params = {
-        'train_data_path':'train_data.csv', #训练数据路径
-        'test_data_path':'test_data.csv'    #测试数据路径
+        'train_data_path':'train_data.csv',             #训练数据路径
+        'test_data_path':'test_data.csv'                #测试数据路径
 }
-model_save_path = 'model.pth'        #模型保存路径
-num_cols = ['月收入', '金融总额', '融资期限']   #数值型特征
-cat_cols = ['婚姻状况', '所在行业', '房产情况']   #分类特征
-label_cols = ['客户ID','是否超期']      #标签
-seed = 42                                #随机种子
-log_interval = 100                       #记录间隔
+model_save_path = 'model.pth'                           #模型保存路径
+num_cols = ['月收入', '金融总额', '融资期限']               #数值型特征
+cat_cols = ['婚姻状况', '所在行业', '房产情况']             #分类特征
+label_cols = ['客户ID','是否超期']                        #标签
+seed = 42                                               #随机种子
+log_interval = 100                                      #记录间隔
